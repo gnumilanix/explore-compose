f083d0af05c94309195f8a79a4a8bc246161dc02
milan <milan@logictechno.com>
2023-04-15 07:48:08 +0800

    test: delete unnecessary test

--- app/src/androidTest/java/com/ignitetech/compose/ui/permissions/PermissionsDialogDeniedTest.kt
+++ app/src/androidTest/java/com/ignitetech/compose/ui/permissions/PermissionsDialogDeniedTest.kt
@@ -2,10 +2,7 @@ package com.ignitetech.compose.ui.permissions
 
 import android.app.Activity
 import android.app.Instrumentation
-import android.content.Intent
-import android.graphics.Bitmap
 import android.net.Uri
-import android.provider.MediaStore
 import android.provider.Settings
 import androidx.compose.ui.test.*
 import androidx.test.espresso.intent.Intents.intended
@@ -14,14 +11,10 @@ import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasData
 import androidx.test.espresso.intent.rule.IntentsRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.ignitetech.compose.R
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import io.mockk.impl.annotations.MockK
-import io.mockk.junit4.MockKRule
 import org.hamcrest.CoreMatchers.allOf
-import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -115,41 +108,5 @@ class PermissionsDialogDeniedTest2 : PermissionsDialogTest() {
         verifyDialogDismissed()
 
         intended(intentMatcher)
-    }
-}
-
-@OptIn(ExperimentalPermissionsApi::class)
-@RunWith(AndroidJUnit4::class)
-@HiltAndroidTest
-class PermissionsDialogDeniedTest3 : PermissionsDialogTest() {
-    @get:Rule(order = 1)
-    var hiltTestRule = HiltAndroidRule(this)
-
-    @get:Rule(order = 2)
-    val intentsTest = IntentsRule()
-
-    @get:Rule(order = 3)
-    val mockkRule = MockKRule(this)
-
-    @MockK
-    lateinit var bitmap: Bitmap
-
-    @Test
-    fun launchesActivityResultLauncherWhenPermissionGranted() {
-        var resultBitmap: Bitmap? = null
-        val resultData = Intent().putExtra("data", bitmap)
-
-        intending(hasAction(MediaStore.ACTION_IMAGE_CAPTURE))
-            .respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, resultData))
-
-        setScreen {
-            resultBitmap = it
-        }
-
-        // Show rationale
-        verifyDialogDismissed()
-
-        // Verify result
-        assertEquals(resultBitmap, bitmap)
     }
 }
